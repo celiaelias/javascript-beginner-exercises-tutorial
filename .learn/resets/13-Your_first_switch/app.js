7b83bfadb5394321203ca825458bc75e59b05f1d
Celia Elias <148253357+celiaelias@users.noreply.github.com>
2023-12-20 17:30:58 +0000

    javascript exercises

--- .learn/resets/13-Your_first_switch/app.js
+++ .learn/resets/13-Your_first_switch/app.js
@@ -2,19 +2,13 @@ function getColor(selection)
 {
 	switch(selection){
 		// Add more options here
-		case 'red':
-			return true;
-		case 'green':
-			return true;
-		case 'blue':
-			return true;
 	    default:
 	    	return false;  //returns false because the user picked an unavailable color
 	    break;               
 	}
 }
 
-let colorname = prompt('What color do you want?').trim().toLowerCase();
+let colorname = prompt('What color do you want?').trim();
 let isAvailable = getColor(colorname);
 
 if(isAvailable)

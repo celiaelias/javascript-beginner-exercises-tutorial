9241dc01e00ab5e732ff3f0776c765a550bde1f2
Celia Elias <148253357+celiaelias@users.noreply.github.com>
2023-12-22 20:39:44 +0000

    javascript exercises

--- exercises/20-Looping_with_FizzBuzz/app.js
+++ exercises/20-Looping_with_FizzBuzz/app.js
@@ -1,5 +1,19 @@
 function fizzBuzz() {  
 	// Your code here
+	for (let i = 1; i < 101; i++) {
+		let text = "";
+		if(i % 3 == 0){
+			text = "Fizz";
+		}
+		if(i % 5 == 0){
+			text = text + "Buzz";
+		}
+		if(text == ""){
+			text = i;
+		}
+		console.log(text);
+
+	}
 }
 
 fizzBuzz();
--- exercises/22-The_Beatles/app.js
+++ exercises/22-The_Beatles/app.js
@@ -1,4 +1,15 @@
-
+function sing() {
+    let text = ""
+    for(let i = 0; i < 4; i++) {
+        text = text + "let it be, "
+    }
+    text = text + "there will be an answer, "
+    for(let i = 0; i < 5; i++) {
+        text = text + "let it be, "
+    }
+    text = text + "whisper words of wisdom, let it be"
+    return text;
+}
 
 //Your code above ^^^
 
